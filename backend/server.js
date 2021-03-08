98ba60de3c00afe4046750670328e012e2b5cc93
Ufere Kalu <dekalusha@gmail.com>
2021-03-07 22:45:22 -0600

    added server connection to MongoDB

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db.js')
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(morgan('dev'))
+}
+
+app.use(express.json())
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+if (process.env.NODE_ENV ==='production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+)
